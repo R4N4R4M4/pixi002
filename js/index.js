8d821cb23deb42fcdec731ad6ccb9ae84d9cab00
ranarama69 <ranarama69@gmail.com>
2016-01-08 21:26:21 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,36 +1,14 @@
 var app = {
     initialize: function() {this.bindEvents();},
     bindEvents: function() {document.addEventListener('deviceready', this.onDeviceReady, false);},
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-		GAME_WIDTH = 800;
-		GAME_HEIGHT = 600;
-		width = window.outerWidth,
-		height = window.outerHeight/2;
-
-		var rendererOptions = {antialias:true, transparent:false, resolution:1,autoresize:true,backgroundColor: 0x008000}
-        var renderer = PIXI.autoDetectRenderer(width, height, rendererOptions);
-		document.getElementById("test").appendChild(renderer.view);
-        var stage = new PIXI.Container();
-
-        var ball = new PIXI.Graphics();
-        ball.beginFill(0xff2200); // Red
-        ball.drawCircle(width/2, height/2, 40); // drawCircle(x, y, radius)
-        ball.endFill();
-		stage.addChild(ball);
-		renderer.render(stage);
-    },
-
+    onDeviceReady: function (){pixi002();},
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
-
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 		document.getElementById("ok").setAttribute('style','display:inline;');		
-		//position();
-
         console.log('Received Event: ' + id);
     }
 };
