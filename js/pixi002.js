4f26d70270930b1f002316629dbd03651a5abeff
ranarama69 <ranarama69@gmail.com>
2016-01-08 21:28:58 +0100

    Update pixi002.js

--- js/pixi002.js
+++ js/pixi002.js
@@ -1,23 +1,26 @@
 function pixi002()
   {
-  app.receivedEvent('deviceready');
+	app.receivedEvent('deviceready');
 	GAME_WIDTH = 800;
 	GAME_HEIGHT = 600;
 	width = window.outerWidth,
 	height = window.outerHeight/2;
-
+	
 	var rendererOptions = {antialias:true, transparent:false, resolution:1,autoresize:true,backgroundColor: 0x008000}
-  var renderer = PIXI.autoDetectRenderer(width, height, rendererOptions);
+	var renderer = PIXI.autoDetectRenderer(width, height, rendererOptions);
 	document.getElementById("test").appendChild(renderer.view);
-  var stage = new PIXI.Container();
-
-  var ball = new PIXI.Graphics();
-  ball.beginFill(0xff2200); // Red
-  ball.drawCircle(width/2, height/2, 40); // drawCircle(x, y, radius)
-  ball.endFill();
+	var stage = new PIXI.Container();
+	
+	var ball = new PIXI.Graphics();
+	ball.beginFill(0xff2200); // Red
+	ball.drawCircle(width/2, height/2, 40); // drawCircle(x, y, radius)
+	ball.endFill();
 	stage.addChild(ball);
 	renderer.render(stage);
+	requestAnimFrame(animate);
 	}
+
+function animate() {requestAnimFrame(animate); renderer.render(stage);}
 
 function position()
 	{
